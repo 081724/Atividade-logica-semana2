9f2aa06386ad81b0f2fa581bf2ff1243e8aeb767
unknown <alexsandrapcampos03@gmail.com>
2022-04-15 15:51:41 -0300

    exemplo tres concluido com sucesso

---
+++ exemplo3.js
@@ -0,0 +1,5 @@
+//3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+const valorJantar = 80
+const taxaGarson = 10/100
+const valorTotal = valorJantar+(taxaGarson*valorJantar)
+console.log("esse e o valor total pago pelo jantar: "+valorTotal)
