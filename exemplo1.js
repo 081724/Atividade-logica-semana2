4edc39cdb9aa1f6955c3d8f28173bb9de949b8eb
unknown <alexsandrapcampos03@gmail.com>
2022-04-15 14:25:43 -0300

    conclui logica do exercicio1

---
+++ exemplo1.js
@@ -0,0 +1,11 @@
+//1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
+
+ const calca = 50
+ const blusa = 25
+ const desconto = 0.2
+
+ const valorDesconto = blusa*desconto
+ const valorPagar = blusa-valorDesconto
+
+ console.log("valor Desconto:" + valorDesconto)
+ console.log("Valor a Pagar:" + valorPagar)
