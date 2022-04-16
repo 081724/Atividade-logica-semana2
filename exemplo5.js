11e8291975de0cbfb46f30aa20952961dde481da
unknown <alexsandrapcampos03@gmail.com>
2022-04-15 23:05:03 -0300

    criei logica do exemplo5

---
+++ exemplo5.js
@@ -0,0 +1,13 @@
+function media (numUm, numDois, numTres) {
+    var resultadoMedia = (numUm+numDois+numTres)/3
+    return resultadoMedia
+}
+
+console.log( media(8, 10, 20) )
+
+const valor1 = 8
+const valor2 = 10
+const valor3 = 20
+
+const media1 = (valor1+valor2+valor3)/3
+console.log("media: " +media1)
